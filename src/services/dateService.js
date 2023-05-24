691d1bc617c54b9a0c6af4dea00751da1f86a08a
savioferraz <savio.ferraz88@gmail.com>
2023-05-24 18:55:52 -0300

    feat: packs products update

--- src/services/dateService.js
+++ src/services/dateService.js
@@ -1,7 +1,10 @@
 const getCurrentDate = () => {
-  const date = new Date().toLocaleDateString("pt-BR");
+  const date = new Date();
+  const year = date.getFullYear();
+  const month = (date.getMonth() + 1).toString().padStart(2, "0");
+  const day = date.getDate().toString().padStart(2, "0");
 
-  return date;
+  return `${year}-${month}-${day}`;
 };
 
 module.exports = {
